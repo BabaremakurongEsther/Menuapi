eae42a9dbf77cd2c96e9a5b5fde862da0d6390d4
BabaremakurongEsther <90789541+BabaremakurongEsther@users.noreply.github.com>
2022-04-21 16:41:59 +0000

    menuapi

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const morgan = require("morgan")
+require("dotenv").config()
+const connectDB = require("./config/connectDB")
+const foodRoute = require("./routes/foodRoute")
+const userRoute = require("./routes/userRoute")
+
+const app = express()
+
+connectDB()
+//Middlewares
+app.use(express.json())
+app.use(morgan("dev"))
+app.use(foodRoute)
+app.use(userRoute)
+
+const PORT = process.env.PORT||9000
+//Home route
+app.get("/", (req, res)=>{
+    res.json("Welcome to My Menu API")
+})
+
+app.listen(PORT, ()=>{
+    console.log("Server is UP!!!")
+})
