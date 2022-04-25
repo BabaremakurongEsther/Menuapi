7d06289fe07dc263370880c2299b312238e370aa
BabaremakurongEsther <90789541+BabaremakurongEsther@users.noreply.github.com>
2022-04-25 15:00:04 +0000

    made some changes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,9 @@
 const User = require("../models/userSchema")
 const bcrypt =require("bcryptjs")
 const validate = require("../config/validator");
+const { use } = require("express/lib/router");
+const req = require("express/lib/request");
+const{generateToken}=require("../util/generateToken")
 
 
 //create a new user
@@ -11,16 +14,21 @@ const valid = await validate({username, email,password})
 
 if (valid) {
 const hashedPassword = await bcrypt.hash(valid.password, 10)
-const user = new User({
+const user = await User.create({
     username,
     email,
     password:hashedPassword,
 });
-await user.save();
-res.status(201).json({
-    message: "User created successful",
-    user,
-});
+if(user){
+    res.status(201).json({
+    username:user.username,
+email:user.email,
+id:user.id,
+token:generateToken(user._id),
+        
+    });
+}
+
 
 }else{
     res.status(400).json({
